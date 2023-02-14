c8bdb85e1a630ffdcb9f02c1514db8a45a73a3ea
BOROWIEC Clément <clement.borowiec@hubone.fr>
2023-02-14 15:49:52 +0100

    j'ai mis le get qui fonctionne

--- backend_clement/get_emplyer.py
+++ backend_clement/get_emplyer.py
@@ -1,15 +1,27 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+
+employees = [
+        {'id': 1, 'firstName': 'Sanchez', 'lastName': 'Alexis', 'emailId': 'alxissanchez@example.com'},
+        {'id': 2, 'firstName': 'Dimitry', 'lastName': 'Payet', 'emailId': 'dimitrypayet@example.com'},
+        {'id': 3, 'firstName': 'Johnatan', 'lastName': 'Clauss', 'emailId': 'johnatanclauss@example.com'}
+    ]
+
 @app.route('/api/v1/employees', methods=['GET'])
 def get_employees():
-    employees = [
-        {'id': 1, 'firstName': 'John', 'lastName': 'Doe', 'emailId': 'johndoe@example.com'},
-        {'id': 2, 'firstName': 'Jane', 'lastName': 'Smith', 'emailId': 'janesmith@example.com'},
-        {'id': 3, 'firstName': 'Bob', 'lastName': 'Johnson', 'emailId': 'bobjohnson@example.com'}
-    ]
     return jsonify(employees)
+
+@app.route('/api/v1/employees', methods=['POST'])
+def add_employee():
+    new_employee = request.json
+    new_employee['id'] = len(employees) + 1
+    employees.append(new_employee)
+    return jsonify(new_employee), 201
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
+
+
+
